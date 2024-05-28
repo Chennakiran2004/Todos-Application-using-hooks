53f1e3cc7fbb9ed221fcf52b6ab7eefcd99dbca7
Chennakiran2004 <kirankumarchenna2004@gmail.com>
2024-05-28 15:28:55 +0530

    commit"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Routes instead of Switch
 import Todo from "./Components/Todo";
-// import NotFound from "./NotFound";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        {" "}
-        {/* Use Routes instead of Switch */}
-        <Route exact path="/" element={<Todo />} />{" "}
-        {/* Use element prop instead of component */}
-        {/* <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-    </Router>
+    <>
+      <Todo />
+    </>
   );
 };
 
